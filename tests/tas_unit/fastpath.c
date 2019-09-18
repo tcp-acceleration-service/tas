9d5ee78c83a6defb79994d6125152d6e00fa112a
Antoine Kaufmann <antoinek@mpi-sws.org>
2019-09-18 15:26:56 +0200

    tests/tas_unit/fastpath: add tests for rx bumps

--- tests/tas_unit/fastpath.c
+++ tests/tas_unit/fastpath.c
@@ -34,10 +34,25 @@ struct flextcp_pl_mem *fp_state = &state_base;
 
 struct dataplane_context **ctxs = NULL;
 
+struct qman_set_op {
+  int got_op;
+  uint32_t id;
+  uint32_t rate;
+  uint32_t avail;
+  uint16_t max_chunk;
+  uint8_t flags;
+} qm_set_op;
+
 int qman_set(struct qman_thread *t, uint32_t id, uint32_t rate, uint32_t avail,
     uint16_t max_chunk, uint8_t flags)
 {
-  printf("qman_set()\n");
+  qm_set_op.got_op = 1;
+  qm_set_op.id = id;
+  qm_set_op.rate = rate;
+  qm_set_op.avail = avail;
+  qm_set_op.max_chunk = max_chunk;
+  qm_set_op.flags = flags;
+
   return 0;
 }
 
@@ -78,6 +93,88 @@ static struct rte_mbuf *mbuf_alloc(void)
   return tmb;
 }
 
+void test_txbump_small(void *arg)
+{
+  int ret;
+  struct flextcp_pl_flowst *fs = &state_base.flowst[0];
+  struct dataplane_context ctx;
+  memset(&ctx, 0, sizeof(ctx));
+
+  flow_init(0, 1024, 1024, 123456);
+
+  struct rte_mbuf *tmb = mbuf_alloc();
+
+  ret = fast_flows_bump(&ctx, 0, 0, 0, 32, 0, (struct network_buf_handle *) tmb, 0);
+  test_assert("unused tx buffer", ret == -1);
+  test_assert("updated tx avail", fs->tx_avail == 32);
+  test_assert("qman set sent", qm_set_op.got_op);
+  test_assert("qman set id correct", qm_set_op.id == 0);
+  test_assert("qman set rate correct", qm_set_op.rate == fs->tx_rate);
+  test_assert("qman set avail correct", qm_set_op.avail == 32);
+  test_assert("qman set max chunk correct", qm_set_op.max_chunk == 1448);
+  test_assert("qman set flags", qm_set_op.flags ==
+      (QMAN_SET_RATE | QMAN_SET_MAXCHUNK | QMAN_ADD_AVAIL));
+}
+
+void test_txbump_full(void *arg)
+{
+  int ret;
+  struct flextcp_pl_flowst *fs = &state_base.flowst[0];
+  struct dataplane_context ctx;
+  memset(&ctx, 0, sizeof(ctx));
+
+  flow_init(0, 1024, 1024, 123456);
+
+  struct rte_mbuf *tmb = mbuf_alloc();
+
+  ret = fast_flows_bump(&ctx, 0, 0, 0, 1024, 0, (struct network_buf_handle *) tmb, 0);
+  test_assert("unused tx buffer", ret == -1);
+  test_assert("updated tx avail", fs->tx_avail == 1024);
+  test_assert("qman set sent", qm_set_op.got_op);
+  test_assert("qman set id correct", qm_set_op.id == 0);
+  test_assert("qman set rate correct", qm_set_op.rate == fs->tx_rate);
+  test_assert("qman set avail correct", qm_set_op.avail == 1024);
+  test_assert("qman set max chunk correct", qm_set_op.max_chunk == 1448);
+  test_assert("qman set flags", qm_set_op.flags ==
+      (QMAN_SET_RATE | QMAN_SET_MAXCHUNK | QMAN_ADD_AVAIL));
+}
+
+void test_txbump_toolong(void *arg)
+{
+  int ret;
+  struct flextcp_pl_flowst *fs = &state_base.flowst[0];
+  struct dataplane_context ctx;
+  memset(&ctx, 0, sizeof(ctx));
+
+  flow_init(0, 1024, 1024, 123456);
+
+  struct rte_mbuf *tmb = mbuf_alloc();
+
+  ret = fast_flows_bump(&ctx, 0, 0, 0, 2048, 0, (struct network_buf_handle *) tmb, 0);
+  test_assert("unused tx buffer", ret == -1);
+  test_assert("updated tx avail", fs->tx_avail == 0);
+  test_assert("qman set sent", !qm_set_op.got_op);
+}
+
+void test_rxbump_toolong(void *arg)
+{
+  int ret;
+  struct flextcp_pl_flowst *fs = &state_base.flowst[0];
+  struct dataplane_context ctx;
+  memset(&ctx, 0, sizeof(ctx));
+
+  flow_init(0, 1024, 1024, 123456);
+  fs->rx_avail = 0;
+
+  struct rte_mbuf *tmb = mbuf_alloc();
+
+  ret = fast_flows_bump(&ctx, 0, 0, 2048, 0, 0, (struct network_buf_handle *) tmb, 0);
+  test_assert("unused tx buffer", ret == -1);
+  test_assert("updated tx avail", fs->rx_avail == 0);
+  test_assert("qman set sent", !qm_set_op.got_op);
+}
+
+
 /* Test rx bump where the flow control window opens up from zero with no tx data
  * available. In this case the fast path should issue an ack to open up the flow
  * control window.
@@ -100,6 +197,7 @@ void test_rxbump_fc_reopen_notx(void *arg)
   test_assert("tx buf in tx queue",
       ctx.tx_handles[0] == (struct network_buf_handle *) tmb);
   test_assert("rx avail updated", fs->rx_avail == 1024);
+  test_assert("qman set sent", !qm_set_op.got_op);
   /* TODO: check ack packet */
 }
 
@@ -124,6 +222,7 @@ void test_rxbump_fc_reopen_tx(void *arg)
   test_assert("used tx buffer", ret == -1);
   test_assert("tx queue num done", ctx.tx_num == 0);
   test_assert("rx avail updated", fs->rx_avail == 1024);
+  test_assert("qman set sent", !qm_set_op.got_op);
 }
 
 /* Test rx bump where the flow control window opens up from zero with tx data
@@ -150,6 +249,7 @@ void test_rxbump_fc_reopen_deadlock(void *arg)
   test_assert("tx buf in tx queue",
       ctx.tx_handles[0] == (struct network_buf_handle *) tmb);
   test_assert("rx avail updated", fs->rx_avail == 1024);
+  test_assert("qman set sent", !qm_set_op.got_op);
   /* TODO: check ack packet */
 }
 
@@ -157,6 +257,18 @@ void test_rxbump_fc_reopen_deadlock(void *arg)
 int main(int argc, char *argv[])
 {
   int ret = 0;
+
+  if (test_subcase("tx bump small", test_txbump_small, NULL))
+    ret = 1;
+
+  if (test_subcase("tx bump full", test_txbump_full, NULL))
+    ret = 1;
+
+  if (test_subcase("tx bump too long", test_txbump_toolong, NULL))
+    ret = 1;
+
+  if (test_subcase("rx bump too long", test_rxbump_toolong, NULL))
+    ret = 1;
 
   if (test_subcase("rx bump fc reopen no tx", test_rxbump_fc_reopen_notx, NULL))
     ret = 1;
