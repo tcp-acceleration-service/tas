5f3043d61f8335fabab95c5321e1ba55b2dfdfff
Rajath Shashidhara <rajaths@cs.utexas.edu>
2021-06-22 18:31:49 +0000

    init flow steering even when autoscaling disabled
    Signed-off-by: Rajath Shashidhara <rajaths@cs.utexas.edu>

--- tas/fast/network.h
+++ tas/fast/network.h
@@ -187,15 +187,15 @@ static inline uint16_t network_buf_tcpxsums(struct network_buf_handle *bh, uint8
 }
 
 static inline int network_buf_flowgroup(struct network_buf_handle *bh,
-    uint16_t *fg)
+    uint16_t *fg, uint16_t core)
 {
   struct rte_mbuf *mb = (struct rte_mbuf *) bh;
   if (!(mb->ol_flags & PKT_RX_RSS_HASH)) {
-    *fg = 0;
+    *fg = core;
     return 0;
   }
 
-  *fg = mb->hash.rss & (rss_reta_size - 1);
+  *fg = mb->hash.rss & (rss_reta_size - 1); // NOTE: assume rte_is_power_of_2(rss_reta_size)
   return 0;
 }
 
