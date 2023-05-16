4c25a289d0306d4c2766dfeb3c8127fcda0fee73
stolet <matheusstolet@hotmail.com>
2023-05-16 19:33:20 +0200

    change TCP_MSS

--- tas/fast/internal.h
+++ tas/fast/internal.h
@@ -62,7 +62,7 @@ extern volatile unsigned fp_scale_to;
 #define QMAN_SET_AVAIL    (1 << 3)
 #define QMAN_ADD_AVAIL    (1 << 4)
 
-#define TCP_MSS 1448
+#define TCP_MSS 1422
 
 /** Node in a list of out of budget VMs to be activated */
 struct out_of_budget_vm {
