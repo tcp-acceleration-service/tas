8db9be9eba1013138c1a23fa1255c0d8a671a338
stolet <matheusstolet@hotmail.com>
2023-05-23 18:20:04 +0200

    changes to overhead throughput experiment

--- experiments/exps/overhead_throughput/overhead_throughput.py
+++ experiments/exps/overhead_throughput/overhead_throughput.py
@@ -2,9 +2,8 @@ import experiments as exp
 
 from exps.overhead_throughput.configs.bare_tas import Config as TasBareConf
 from exps.overhead_throughput.configs.bare_vtas import Config as VTasBareConf
+from exps.overhead_throughput.configs.bare_vtas_tunoff import Config as VTasBareTunOffConf
 from exps.overhead_throughput.configs.virt_tas import Config as TasVirtConf
-from exps.overhead_throughput.configs.ovs_linux import Config as OVSLinuxConf
-from exps.overhead_throughput.configs.ovs_tas import Config as OVSTasConf
 
 experiments = []
 
@@ -16,13 +15,11 @@ for n_r in range(n_runs):
     exp_name = "overhead-throughput-run{}-msize{}_".format(n_r, n_m)
     tas_bare_exp = exp.Experiment(TasBareConf(exp_name + "bare-tas", n_m), name=exp_name)
     vtas_bare_exp = exp.Experiment(VTasBareConf(exp_name + "bare-vtas", n_m), name=exp_name)
+    vtas_bare_tunoff_exp = exp.Experiment(VTasBareTunOffConf(exp_name + "bare-tunoffvtas", n_m), name=exp_name)
     tas_virt_exp = exp.Experiment(TasVirtConf(exp_name + "virt-tas", n_m), name=exp_name)
-    ovs_linux_exp = exp.Experiment(OVSLinuxConf(exp_name + "ovs-linux", n_m), name=exp_name)
-    ovs_tas_exp = exp.Experiment(OVSTasConf(exp_name + "ovs-tas", n_m), name=exp_name)
 
     experiments.append(tas_bare_exp)
+    experiments.append(vtas_bare_tunoff_exp)
     experiments.append(vtas_bare_exp)
     experiments.append(tas_virt_exp)
-    # experiments.append(ovs_tas_exp)
-    # experiments.append(ovs_linux_exp)
   
--- experiments/exps/overhead_throughput/plot_script.plt
+++ experiments/exps/overhead_throughput/plot_script.plt
@@ -1,17 +1,18 @@
 #!/usr/bin/gnuplot -persist
-set terminal pdf
+set terminal pdf font "Latin Modern Roman"
 set output "plot.pdf"
 set key autotitle columnhead
 set key left top
 set logscale x 2
-set bmargin 4
               
-set key top right
-set label 1 "Message Size [Bytes]" at screen 0.5, 0.03 center
-set label 2 "Throughput [Mbps]" at screen 0.01, 0.5 rotate by 90 center
+set key top left
+set xlabel "Message Size [Bytes]"
+set ylabel "Throughput [Mbps]"
+set yrange [0:]
 
-plot 'tp.dat' using 1:2:5 with yerrorlines title 'tas' ps 0.7, \
-     'tp.dat' using 1:3:6 with yerrorlines title 'bare-virtuoso' ps 0.7, \
-     'tp.dat' using 1:4:7 with yerrorlines title 'virtuoso' ps 0.7, \
+plot 'tp.dat' using 1:2:6 with yerrorlines title 'tas', \
+     'tp.dat' using 1:3:7 with yerrorlines title 'bare-virtuoso', \
+     'tp.dat' using 1:4:8 with yerrorlines title 'virtuoso', \
+     'tp.dat' using 1:5:9 with yerrorlines title 'tunoff-virtuoso', \
      # 'tp.dat' using 1:5:9 with yerrorlines title 'ovs-linux' linetype 5 ps 0.7, \
      # 'tp.dat' using 1:6:11 with yerrorlines title 'ovs-tas' linetype 6 ps 0.7, \
