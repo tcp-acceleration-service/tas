0f1196ad3a54711025de4d1d5cc82aff53e693eb
stolet <matheusstolet@hotmail.com>
2023-06-14 22:56:57 +0200

    make total memory equal RAM size in vm start script

--- images/start-vm.sh
+++ images/start-vm.sh
@@ -71,7 +71,7 @@ elif [[ "$stack" == 'ovs-linux' ]]; then
     -chardev socket,id=char0,path=/usr/local/var/run/openvswitch/$vhost \
     -netdev type=vhost-user,chardev=char0,vhostforce=on,queues=12,id=net1 \
     -device virtio-net-pci,netdev=net1,mac=$alt_mac,mq=on,vectors=26 \
-    -object memory-backend-file,id=mem,size=25G,mem-path=/dev/hugepages,share=on \
+    -object memory-backend-file,id=mem,size=10G,mem-path=/dev/hugepages,share=on \
     -numa node,memdev=mem -mem-prealloc \
     -drive if=virtio,format=qcow2,file="base.snapshot.qcow2" \
     -drive if=virtio,format=raw,file="seed.img" \
@@ -89,7 +89,7 @@ elif [[ "$stack" == 'ovs-tas' ]]; then
     -chardev socket,id=char0,path=/usr/local/var/run/openvswitch/$vhost \
     -netdev type=vhost-user,chardev=char0,vhostforce=on,queues=12,id=net1 \
     -device virtio-net-pci,netdev=net1,mac=$alt_mac,mq=on,vectors=26 \
-    -object memory-backend-file,id=mem,size=25G,mem-path=/dev/hugepages,share=on \
+    -object memory-backend-file,id=mem,size=10G,mem-path=/dev/hugepages,share=on \
     -numa node,memdev=mem -mem-prealloc \
     -drive if=virtio,format=qcow2,file="base.snapshot.qcow2" \
     -drive if=virtio,format=raw,file="seed.img" \
